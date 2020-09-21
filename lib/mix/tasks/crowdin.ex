f98b1ea545056e77533f52d9f7f928de68a0b75a
Wei Huang <weih@kahoot.com>
2020-09-21 14:30:58 +0200

    Extra find matching remote file

--- lib/mix/tasks/crowdin.ex
+++ lib/mix/tasks/crowdin.ex
@@ -11,15 +11,20 @@ defmodule Mix.Tasks.Crowdin do
     |> IO.inspect
   end
 
+  def find_matching_remote_file(client, project_id, source_name) do
+    with {:ok, res} <- Crowdin.list_files(client, project_id),
+      200 <- res.status do
+      Enum.find(res.body["data"], fn file -> file["data"]["name"] == source_name end)
+    end
+  end
+
   def upload_source(workspace, client, project_id, source_file) do
     path = Path.join(workspace, source_file)
     source_name = Path.basename(source_file)
     with {:ok, res} <- Crowdin.add_storage(client, path),
          201 <- res.status,
-         %{"data" => %{"id" => storage_id}} <- res.body,
-         {:ok, res} <- Crowdin.list_files(client, project_id),
-         200 <- res.status do
-      case Enum.find(res.body["data"], fn file -> file["data"]["name"] == source_name end) do
+         %{"data" => %{"id" => storage_id}} <- res.body do
+      case find_matching_remote_file(client, project_id, source_name) do
         nil -> Crowdin.add_file(client, project_id, storage_id, source_name)
         file -> Crowdin.update_file(client, project_id, file["data"]["id"], storage_id)
       end
