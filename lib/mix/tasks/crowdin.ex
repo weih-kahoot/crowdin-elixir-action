89223e8183cd0e18332977669b72bd5680334dbf
Wei Huang <weih@kahoot.com>
2020-09-21 12:19:59 +0200

    Initial project

---
+++ lib/mix/tasks/crowdin.ex
@@ -0,0 +1,10 @@
+defmodule Mix.Tasks.Crowdin do
+  use Mix.Task
+
+  def run(_) do
+    # This will start our application
+#    Mix.Task.run("app.start")
+
+    IO.puts "Corwdin mix task"
+  end
+end
